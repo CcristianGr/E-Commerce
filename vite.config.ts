20d921aa68cffed933a3fa236b8b0ddf1c83d4fd
CCGR <cristiancamilogonzalezrios@gmail.com>
2026-02-03 15:40:44 -0500

    Configurar nombres de archivo fijos para assets en producción

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),],
   base: '/React-Project/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
