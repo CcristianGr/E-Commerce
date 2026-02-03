5dd1c93398d0166a0073d7ab3a2502f6e2d0436f
CCGR <cristiancamilogonzalezrios@gmail.com>
2026-02-03 09:39:46 -0500

    Creando Componente CheckOutSideMenu, encargado de mostrar los productos en el carrito, y haciendo ajustes para el renderizado de las cards

--- src/Components/Cards/Cards.tsx
+++ src/Components/Cards/Cards.tsx
@@ -3,10 +3,10 @@ import { ShoppingCartContext } from "../../Context/ShoppingContext"
 import { PlusIcon } from '@heroicons/react/24/solid'
 
 interface CardsProps {
-  data: any[]
+  data: any
 }
 
-export const Cards = ({ data = [] }: CardsProps) => {
+export const Cards = ({ data }: CardsProps) => {
     const context = useContext(ShoppingCartContext)
 
     const showProduct = (product: any) => {
@@ -14,39 +14,35 @@ export const Cards = ({ data = [] }: CardsProps) => {
         context.openProductDetail()
     }
 
-    const addProductsToCard = (productData: any) => {
+    const addProductsToCard = (event:any, productData: any) => {
         const current = context.cartProducts ?? []
+
+        event.stopPropagation()
         context.setCount(context.count + 1)
         context.setCartProducts([...current, productData])
+        context.openCheckOutSideMenu()
     }
 
     return (
-        <div className="grid grid-cols-4 gap-4 p-4">
-            {
-                data?.map((item: any, index: number) => (
-                    <div className=" w-56 h-60 rounded-lg p-4 flex flex-col justify-" key={item.id ?? index}>
-                        <figure className="relative w-full h-4/5">
-                            <div 
-                                className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 cursor-pointer"
-                                onClick={(e) => { e.stopPropagation(); addProductsToCard(item); }}>
-                                   <PlusIcon className="h-4 w-4" />
-                            </div>
-                            <img 
-                                src={item.images?.[0]} 
-                                alt={item.title} 
-                                className="w-full h-full object-cover rounded-lg cursor-pointer"
-                                onClick={() => showProduct(item)}
-                            />
-                            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{item.category?.name}</span>
-                        </figure>
-                        <p className="flex justify-between">
-                            <span>{item.title}</span>
-                            <span>${item.price}</span>
-                        </p>
-
-                    </div>
-                ))
-            }
+        <div className="w-56 h-60 rounded-lg p-4 flex flex-col justify-">
+            <figure className="relative w-full h-4/5">
+                <div 
+                    className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 cursor-pointer"
+                    onClick={(e) => addProductsToCard(e, data)}>
+                       <PlusIcon className="h-4 w-4" />
+                </div>
+                <img 
+                    src={data.images?.[0]} 
+                    alt={data.title} 
+                    className="w-full h-full object-cover rounded-lg cursor-pointer"
+                    onClick={() => showProduct(data)}
+                />
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.category?.name}</span>
+            </figure>
+            <p className="flex justify-between">
+                <span>{data.title}</span>
+                <span>${data.price}</span>
+            </p>
         </div>
     )
 }
--- src/Context/ShoppingContext.tsx
+++ src/Context/ShoppingContext.tsx
@@ -3,13 +3,23 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext<any>(null)
 
 export const ShoppingCartProvider = ({children}: any) => {
+
     const [count, setCount] = useState(0)
+    
+    //Produc Detail
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    //CheckOut Side Menu
+    const [isCheckOutSideMenuOpen, setIsCheckOutSideMenuOpen] = useState(false)
+    const openCheckOutSideMenu = () => setIsCheckOutSideMenuOpen(true)
+    const closeCheckOutSideMenu = () => setIsCheckOutSideMenuOpen(false)
+
+    //Shopping Cart  . Add products to cart
     const [cartProducts, setCartProducts] = useState([])
 
-    // product detail
+    //Product to show in product detail
     const [productToShow, setProductToShow] = useState({})
 
   return (
@@ -22,7 +32,10 @@ export const ShoppingCartProvider = ({children}: any) => {
         openProductDetail,
         closeProductDetail,
         productToShow,
-        setProductToShow
+        setProductToShow,
+        isCheckOutSideMenuOpen,
+        openCheckOutSideMenu,
+        closeCheckOutSideMenu
         }}>
       {children}
     </ShoppingCartContext.Provider>
